693ab5d41f26212abc387561fba82ffb571c5dcd
ext-jenneferl <ext-jenneferl@airg.com>
2026-02-11 09:26:33 +0800

    -fixed css blocking paint

--- app.vue
+++ app.vue
@@ -1,5 +1,3 @@
 <template>
 	<NuxtPage />
 </template>
-
-<style src="./assets/styles.css"></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
+	css: ['~/assets/styles.css'],
+
 	app: {
 		head: {
 			title: 'Image Upload to S3',
